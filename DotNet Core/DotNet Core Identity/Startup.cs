ea09c66871866a353b6cd4c59d44ce502e72d4eb
Edin Jašarević <edinjasar14@gmail.com>
2021-02-08 23:54:47 +0100

    implementing asp.net core identity from scratch

--- DotNet Core/DotNet Core Identity/Startup.cs
+++ DotNet Core/DotNet Core Identity/Startup.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace DotNet_Core_Identity
 {
@@ -24,6 +25,9 @@ namespace DotNet_Core_Identity
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddIdentityCore<PluralsightUser>(options => { });
+            services.AddScoped<IUserStore<PluralsightUser>, PluralsightUserStore>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
