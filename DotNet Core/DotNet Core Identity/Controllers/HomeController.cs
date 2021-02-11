a3f5c165e8ebd05f6d848a41ccaeb10a314f22b6
Edin Jašarević <edinjasar14@gmail.com>
2021-02-11 18:50:30 +0100

    basic user registration

--- DotNet Core/DotNet Core Identity/Controllers/HomeController.cs
+++ DotNet Core/DotNet Core Identity/Controllers/HomeController.cs
@@ -6,15 +6,18 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace DotNet_Core_Identity.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserManager<PluralsightUser> _userManager;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(UserManager<PluralsightUser> userManager, ILogger<HomeController> logger)
         {
+            _userManager = userManager;
             _logger = logger;
         }
 
@@ -32,6 +35,37 @@ namespace DotNet_Core_Identity.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(model.UserName);
+
+                if (user == null)
+                {
+                    user = new PluralsightUser
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        UserName = model.UserName
+                    };
+
+                    var result = await _userManager.CreateAsync(user, model.Password);
+                }
+
+                return View("Success");
+            }
+
+            return View();
         }
     }
 }
